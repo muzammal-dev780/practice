60ceba5d71bbbd7047ca649cd4aceb42c2ef54f5
muzammal <your.email@example.com>
2020-07-21 19:44:10 +0500

    final commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+	protect_from_forgery with: :exception
+	def hello
+		render html: "Hello from muzammal murtaza"
+	end
+	def bye
+		render html: "byebye from muzammal murtaza"
+	end
 end
